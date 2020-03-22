99807f9761fd8fb46a329464054033c1bca3d01f
TrungTran14a <trung199622@gmail.com>
2020-03-23 00:46:04 +0700

    some

--- songf.php
+++ songf.php
@@ -1,30 +1,20 @@
 
-
+	
 				<div class="totalinfo row ">
                 	<div id="tagfilm" class="col-sm-12"><i id="icontag" class="material-icons" >star_border</i>&nbsp songs &nbsp <i id="icontag" class="material-icons" >star_border</i></div> 
+                    <?php include('ketnoibh.php');
+						$get = $_GET['o'];
+						$s="select * from film,song,singer where film.IdF = song.IdF and song.IdSin = singer.IdSin";
+						$kq=mysqli_query($conn,$s);
+						while($d = mysqli_fetch_assoc($kq))
+							{
+						?>
                     <div id="iffilm" class="col-sm-12 infodetail">
-                        <span> <b>Name:</b> uchiage hanabi, shitakara miru ka, yoko kara miru ka.</span>
+                        <span> <b>Song Name:</b> <?php echo $d['NameS'];?></span>
+                        <p></p>
+                        <span> <b>Singer:</b> <?php echo $d['NameSin'];?></span>
                     </div>
-                    <div id="iffilm" class="col-sm-12 infodetail">
-                        <span> <b>Name:</b> uchiage hanabi, shitakara miru ka, yoko kara miru ka.</span>
-                    </div>
-                     <div id="iffilm" class="col-sm-12 infodetail">
-                        <span> <b>Name:</b> uchiage hanabi, shitakara miru ka, yoko kara miru ka.</span>
-                    </div>
-                     <div id="iffilm" class="col-sm-12 infodetail">
-                        <span> <b>Name:</b> uchiage hanabi, shitakara miru ka, yoko kara miru ka.</span>
-                    </div>
-                     <div id="iffilm" class="col-sm-12 infodetail">
-                        <span> <b>Name:</b> uchiage hanabi, shitakara miru ka, yoko kara miru ka.</span>
-                    </div>
-                     <div id="iffilm" class="col-sm-12 infodetail">
-                        <span> <b>Name:</b> uchiage hanabi, shitakara miru ka, yoko kara miru ka.</span>
-                    </div>
-                     <div id="iffilm" class="col-sm-12 infodetail">
-                        <span> <b>Name:</b> uchiage hanabi, shitakara miru ka, yoko kara miru ka.</span>
-                    </div>
-                    <div id="iffilm" class="col-sm-12 infodetail">
-                        <span> <b>Name:</b> uchiage hanabi, shitakara miru ka, yoko kara miru ka.</span>
-                    </div>
+                   
                     
                 </div>
+                <?php } ?>
