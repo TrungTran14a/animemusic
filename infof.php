bb997eeac1c7fc99949ef7c408ba4d6d3e8ca3e1
TrungTran14a <trung199622@gmail.com>
2020-03-24 00:15:06 +0700

    some

--- infof.php
+++ infof.php
@@ -2,7 +2,9 @@
 	 <?php  include("ketnoibh.php");
 			    	
 					$f=$_GET["o"];
-					$s="select * from film where Idf = $f";
+					$s="select * from film,status,type,season,studio,rating 
+						where Idf = $f and film.IdStt = status.IdStt and film.IdType = type.IdType and film.IdSea = season.IdSea 
+						and film.IdStu = studio.IdStu and film.IdRa = rating.IdRa ";
 					$kq=mysqli_query($conn,$s);
 					while($d = mysqli_fetch_assoc($kq))
 					{
@@ -16,10 +18,10 @@
                                      <span> <b>Name English:</b> <?php echo $d['Namee']; ?></span>
                                     </li>
                                     <li>
-                                     <span> <b>Name English:</b> <?php echo $d['Namej']; ?></span>
+                                     <span> <b>Name Japanese:</b> <?php echo $d['Namej']; ?></span>
                                     </li>
                                     <li>
-                                     <span> <b>Name English:</b> <?php echo $d['Namer']; ?></span>
+                                     <span> <b>Name Romanji:</b> <?php echo $d['Namer']; ?></span>
                                     </li>
                                     <li>
                                      <span> <b>Episodes:</b> <?php echo $d['Eps']; ?></span>
@@ -33,8 +35,32 @@
                                     <li>
                                      <span> <b>Views:</b> <?php echo $d['ViewF']; ?></span>
                                     </li>
+                                     <li>
+                                     <span> <b>Status:</b> <?php echo $d['NStt']; ?></span>
+                                    </li>
+                                    <li>
+                                     <span> <b>Type:</b> <?php echo $d['NType']; ?></span>
+                                    </li>
+                                    <li>
+                                     <span> <b>Season:</b> <?php echo $d['NSea']; ?></span>
+                                    </li>
+                                    <li>
+                                     <span> <b>Studio:</b> <?php echo $d['NameStu']; ?></span>
+                                    </li>
+                                    <li>
+                                     <span> <b>Rating:</b> <?php echo $d['Nra']; ?></span>
+                                    </li>
+                                     
+                                     <li>
+                                     <span> <b>Genres:</b>  
+													 <?php $f=$_GET["o"];
+                                                        $sq="select * from film,genres 
+                                                        where Idf = $f and film.IdF = genres.IdFuck";
+                                                        $kql=mysqli_query($conn,$sq);
+                                                        while($dd = mysqli_fetch_assoc($kql))
+                                                        {  echo $dd['NGen'].'.'?>
+                                     </span>     <?php } ?>
+                                    </li>
                                 </ul>
-                            
-                           
                         </div>
-                <?php } ?>
+           <?php } ?>
