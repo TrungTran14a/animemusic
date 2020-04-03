ce08c900e8676b26fb20f64f673ea939b157b5a6
TrungTran14a <trung199622@gmail.com>
2020-04-04 00:17:00 +0700

    more

--- filmwall.php
+++ filmwall.php
@@ -9,9 +9,14 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 <link type="text/css" rel="stylesheet" href="./css/hover/css/hover.css"  media="all">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link rel="stylesheet" href="search/auto/easy-autocomplete.min.css">
+	
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <script src="search/auto/jquery.easy-autocomplete.min.js"></script>
+  <script src="code.jquery.com/jquery-1.11.2.min.js"></script>
+	
 <title>ANIME NAME</title>
 </head>
 
@@ -20,8 +25,10 @@
 <div id="size" class="container">
 	<?php include('mhead.php'); ?>
     <?php include('contentfw.php'); ?>
+    
 </div>
 	<?php include('mend.php') ?>
+	<div id='a'></div>
 
 </body>
 </html>
--- songf.php
+++ songf.php
@@ -14,7 +14,7 @@
                         <p></p>
                         <span> <b>Singer:</b> <?php echo $d['NameSin'];?></span>
                         <p></p>
-                         <audio class="amthanh" controls>
+                         <audio class="amthanh" controls loop >
                           <source src="<?php echo "music/".$d['LinkS']?>" type="audio/ogg">
                           <source src="<?php echo "music/".$d['LinkS']?>" type="audio/mpeg">
                         		Your browser does not support the audio element.
@@ -23,12 +23,6 @@
                    
                     
                 </div>
-                <div id="cp_widget_8325034b-c473-45e6-a7ab-729ed2f82b06">...</div><script type="text/javascript">
-var cpo = []; cpo["_object"] ="cp_widget_8325034b-c473-45e6-a7ab-729ed2f82b06"; cpo["_fid"] = "AkFAToOVplri";
-var _cpmp = _cpmp || []; _cpmp.push(cpo);
-(function() { var cp = document.createElement("script"); cp.type = "text/javascript";
-cp.async = true; cp.src = "//www.cincopa.com/media-platform/runtime/libasync.js";
-var c = document.getElementsByTagName("script")[0];
-c.parentNode.insertBefore(cp, c); })(); </script>
+                
 
                 <?php } ?>
