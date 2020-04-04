b2d1c685c7ed72255ddaa4e43f3dc6beddda82a1
TrungTran14a <trung199622@gmail.com>
2020-04-05 00:54:04 +0700

    more

--- songf.php
+++ songf.php
@@ -14,11 +14,12 @@
                         <p></p>
                         <span> <b>Singer:</b> <?php echo $d['NameSin'];?></span>
                         <p></p>
-                         <audio class="amthanh" controls loop >
+                         <audio id="amthanh" class="amthanh" controls loop >
                           <source src="<?php echo "music/".$d['LinkS']?>" type="audio/ogg">
                           <source src="<?php echo "music/".$d['LinkS']?>" type="audio/mpeg">
                         		Your browser does not support the audio element.
                         </audio> 
+						<button class="dload" onclick="myFunction()">download</button>
                     </div>
                    
                     
@@ -26,3 +27,17 @@
                 
 
                 <?php } ?>
+<script>
+function myFunction() {
+  var li = document.getElementsByTagName('source')[0].src;
+ alert(li);
+  var node = document.createElement("a");
+  node.setAttribute('href',li);
+  node.setAttribute('download','');
+  down = document.createTextNode("shit");
+  node.appendChild(down); 
+  document.getElementById("amthanh").appendChild(node);
+  node.click();
+  document.getElementById("amthanh").removeChild(node);
+}
+</script>
