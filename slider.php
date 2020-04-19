d4b1e1610629bd9b17073e93ef01f9f1920c0c55
TrungTran14a <trung199622@gmail.com>
2020-04-20 00:33:14 +0700

    aaa

--- slider.php
+++ slider.php
@@ -11,12 +11,12 @@
   </ol>
   <div id="allfmsli" class="carousel-inner">
 	  <?php include('ketnoibh.php');
-						  $s = "select * from film,thumb where thumb.IdF = film.IdF  ";
+						  $s = "select * from film,thumb,type,studio where thumb.IdF = film.IdF and  film.IdStu = studio.IdStu and film.IdType = type.IdType";
 						  $kq=mysqli_query($conn,$s);
-						  while($d = mysqli_fetch_assoc($kq))
+						  while($d = mysqli_fetch_array($kq))
 							{
 								?>
-    <div  class="carousel-item">
+    <div  id="slayder" class="carousel-item" data-id="<?php echo $d['IdThu']."s" ?>">
 								<div class="d-block w-100" id="fmsli" style="background-image:url('<?php echo $d['LinkTh']?>');">
 									<div class="infosli" >
 										<div class="infsli">
@@ -28,7 +28,7 @@
 												<li><b>episodes:</b> <?php echo $d['Eps']?></li>
 
 										</div>
-										<div class="btnwatch" >
+										<div class="btnwatch" onClick="dem()">
 											<p1>details</p1>
 										</div>
 									</div>
@@ -39,13 +39,34 @@
 	  
 	   <?php }?>
   </div>
-
- 
+	
+	
+	
+	
+<!-- this script is used for slayder -->
+<script>
+	
+	var a = document.getElementsByClassName('carousel-item')[0];
+	var b = a.getAttribute('data-id');
+	var c = "1s";
+	if(b == c)
+	   {
+		  a.setAttribute( "class", "carousel-item active" );
+	   }
+	
+</script>
+<!-- this script is used for slayder -->
+	
+	
+	
+	
+	
+	
   <a id="mnext" class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
-  <a id="mprev" class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+  <a id="mprev" class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next" onClick="dem()">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
