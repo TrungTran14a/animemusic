ea4475ce5faa2ce8d7e2b67c6e65a7ddadedeb01
TrungTran14a <trung199622@gmail.com>
2021-03-09 15:49:36 +0700

    upgrade search function

--- mbody2.php
+++ mbody2.php
@@ -26,7 +26,7 @@
 					else{ 
 									include("ketnoibh.php");
 									$f=$_GET["search"];
-									$s="select * from film,photo where photo.IdF = film.IdF and Namee Like('%$f%') and (Namer Like('%$f%')) ";
+									$s="select * from film,photo where film.IdF = photo.IdF and  (film.Namee Like('%$f%') or film.Namer Like('%$f%') or film.Namej Like('%$f%'))";
 									$kq=mysqli_query($conn,$s) ;
 									
 									?>
